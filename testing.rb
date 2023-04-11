00f14149333bede64799c5bd92a603c238bdef0f
Fene-87 <fenekayas87@gmail.com>
2023-04-11 16:09:29 +0300

    Learn string methods

---
+++ testing.rb
@@ -0,0 +1,14 @@
+output = puts "Hello World"
+puts output
+
+bar = " my cool "
+foo = " string "
+baz = foo
+baz = baz.concat(" today ")
+foo = bar
+bar = foo.concat(" yesterday #{baz}")
+puts bar
+
+my_name = "   Marko"
+puts "My name is " + my_name
+puts my_name.strip[0]
