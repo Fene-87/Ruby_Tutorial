bf01780604960793928cf3162a840ebe1bc297dc
Fene-87 <fenekayas87@gmail.com>
2023-04-11 17:11:25 +0300

    Math and Numbers

--- testing.rb
+++ testing.rb
@@ -9,6 +9,21 @@ foo = bar
 bar = foo.concat(" yesterday #{baz}")
 puts bar
 
-my_name = "Marko"
+my_name = "Fenekayas"
 puts "My name is " + my_name
+puts "Mark".upcase()
+puts "Mark".include? "j"
+puts "Mark".include? "M"
+puts "Mark".include? "m"
 puts my_name.strip[0, 4]
+puts my_name.index("a")
+
+# Math and Numbers
+
+puts 5 + 9
+puts 2 ** 3
+num = 20
+puts ("My favourite num is " + num.to_s)
+puts num
+puts 10 / 7
+puts 10 / 7.0
